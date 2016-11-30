9ebb7e1c99a4219aea080803cdd3d7b446094b44
Justin Moore <JMoore@JustinMoore-MacbookPro.local>
2016-11-30 09:29:56 -0500

    second commit

--- CPics/CAParse.swift
+++ CPics/CAParse.swift
@@ -1,9 +1,21 @@
-//
-//  CAParse.swift
-//  CPics
-//
-//  Created by Justin Moore on 11/17/16.
-//  Copyright © 2016 Justin Moore. All rights reserved.
-//
-
 import Foundation
+import Parse
+import Bolts
+
+var serverInitialized = false
+
+class ParseServer {
+    static let sharedInstance = ParseServer()
+    private init() {
+        if !serverInitialized {
+            let configuration = ParseClientConfiguration {
+                $0.applicationId = "wx8eMIWy1f9e60WrQJYUI81jlk5g6YYAPPmwxequ"
+                $0.clientKey = "ECyvUjxayFW3un2sOkTkgFJC8mmqweeOAjW0OlKJ"
+                $0.server = "http://corpsboard.herokuapp.com/parse"
+                $0.isLocalDatastoreEnabled = true
+            }
+            Parse.initialize(with: configuration)
+            serverInitialized = true
+        }
+    }
+}
--- CPics/TableViewController.swift
+++ CPics/TableViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Parse
 
-class TableViewController: UITableViewController {
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let Server = ParseServer.sharedInstance
+    var searchQuery = PFQuery(className: "Member")
+    var arrayOfAllMembers: [PFObject]? = nil
+    var arrayOfFilteredMembers: [PFObject]? = nil
+    var memberToOpen: PFObject? = nil
+    var imagePicker: UIImagePickerController!
+    
+    @IBOutlet weak var txtSearch: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        txtSearch.delegate = self
+        txtSearch.addTarget(self, action: #selector(UITextFieldDelegate.textFieldShouldEndEditing(_:)), for: UIControlEvents.editingChanged)
+        
+        refreshServer()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -20,67 +34,173 @@ class TableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool { // do stuff
+        search()
+        return true
+    }
+    
+    @IBAction func takePicture(_ sender: Any) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func refreshServer() {
+        arrayOfAllMembers?.removeAll()
+        let query = PFQuery(className: "Member")
+        query.limit = 1000
+        query.findObjectsInBackground { (members: [PFObject]?, err: Error?) in
+            if members != nil {
+                self.arrayOfAllMembers = members!
+                self.arrayOfFilteredMembers = members!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        var image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        image = fixOrientation(img: image!)
+        let imageData = UIImageJPEGRepresentation(image!, 0.5)
+        
+        //let imageData = UIImagePNGRepresentation(image!)
+        //create a parse file to store in cloud
+        let parseImageFile = PFFile(name: "picture.png", data: imageData!)
+        
+        parseImageFile?.saveInBackground(block: { (success: Bool, error: Error?) in
+            if success {
+                self.memberToOpen?["picture"] = parseImageFile
+                self.memberToOpen?.saveEventually({ (done: Bool, err: Error?) in
+                    self.tableView.reloadData()
+                })
+            }
+        })
+    }
+    
+    
+    func fixOrientation(img:UIImage) -> UIImage {
+        
+        if (img.imageOrientation == UIImageOrientation.up) {
+            return img;
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(img.size, false, img.scale);
+        let rect = CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height)
+        img.draw(in: rect)
+        
+        let normalizedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext();
+        return normalizedImage;
+        
+    }
+
+    @IBAction func setNumber(_ sender: Any) {
+        //1. Create the alert controller.
+        
+        let alert = UIAlertController(title: memberToOpen?["name"] as? String, message: "Set number:", preferredStyle: .alert)
+        
+        //2. Add the text field. You can configure it however you need.
+        alert.addTextField { (textField) in
+            //textField.text = "Some default text."
+             textField.keyboardType = UIKeyboardType.numberPad
+        }
+        
+        // 3. Grab the value from the text field, and print it when the user clicks OK.
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            let textField = alert.textFields![0] // Force unwrapping because we know it exists.
+            self.memberToOpen?.setObject(Int(textField.text!)!, forKey: "number")
+            self.memberToOpen?.saveEventually()
+            self.tableView.reloadData()
+            textField.resignFirstResponder()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (_) in
+            
+        }))
+        
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+     func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfFilteredMembers?.count ?? 0
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        memberToOpen = arrayOfFilteredMembers?[indexPath.row]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellMember", for: indexPath)
+        
+        let lblNumber = cell.viewWithTag(1) as! UILabel
+        let lblName = cell.viewWithTag(2) as! UILabel
+        let lblSections = cell.viewWithTag(3) as! UILabel
+        let imgCorps = cell.viewWithTag(4) as! UIImageView
+        let imgPicture = cell.viewWithTag(5) as! UIImageView
+        
+        
+        if let member = arrayOfFilteredMembers?[indexPath.row] {
+            
+            if (member["picture"] as? PFFile) != nil {
+                imgPicture.isHidden = false
+            } else {
+                imgPicture.isHidden = true
+            }
+            
+            let num = member["number"] as? Int ?? 0
+            if num != 0 {
+                lblNumber.text = "\(num)"
+            } else {
+                lblNumber.text = ""
+            }
+            lblName.text = member["name"] as? String ?? "Error"
+            
+            if let arrayOfInstruments = member["sections"] as! [String]? {
+                var strInstruments = ""
+                if arrayOfInstruments.count > 1 {
+                    for instrument in arrayOfInstruments {
+                        strInstruments += instrument + "    "
+                    }
+                } else if arrayOfInstruments.count == 1 {
+                    strInstruments = arrayOfInstruments.first!
+                }
+                lblSections.text = strInstruments
+            }
+            
+            let cadets = member["cadets"] as! Bool
+            let cadets2 = member["cadets2"] as! Bool
+            if cadets && !cadets2 {
+                imgCorps.image = UIImage(named: "Cadets")
+            } else if !cadets && cadets2 {
+                imgCorps.image = UIImage(named: "Cadets2")
+            } else if cadets && cadets2 {
+                imgCorps.image = UIImage(named: "CadetsCadets2")
+            }
+            
+        } else {
+            lblNumber.text = "Err"
+            lblName.text = ""
+            lblSections.text = ""
+            imgCorps.image = nil
+        }
+        
 
         return cell
     }
-    */
+ 
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -91,5 +211,40 @@ class TableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func addMemberToFilteredArray(member: PFObject) {
+        //make sure they don't exist in filtered array, then add
+        if !(arrayOfFilteredMembers?.contains(member))! {
+            arrayOfFilteredMembers?.append(member)
+        }
+        tableView.reloadData()
+    }
+    
+    func search() {
+        arrayOfFilteredMembers?.removeAll()
+        if txtSearch.text?.characters.count == 0 {
+            arrayOfFilteredMembers = arrayOfAllMembers
+            tableView.reloadData()
+            return
+        }
+        
+        for member in arrayOfAllMembers! {
+            if let num = Int(txtSearch.text!) {
+                if let memnum = member["number"] as? Int {
+                    if memnum == num {
+                        addMemberToFilteredArray(member: member)
+                    }
+                }
+            } else {
+                if let name = member["name"] as? String {
+                    let nameLower = name.lowercased()
+                    let textLower = txtSearch.text?.lowercased()
+                    if nameLower.range(of: textLower!) != nil {
+                        addMemberToFilteredArray(member: member)
+                    }
+                }
+            }
+            tableView.reloadData()
+        }
+    }
 }
